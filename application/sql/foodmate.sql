004b495856e63c260f0dcac164e0e4a31cf34378
Christopher Frew <chris@mawson.tech>
2019-11-12 12:44:18 +0930

    Update foodmate.sql

--- application/sql/foodmate.sql
+++ application/sql/foodmate.sql
@@ -46,7 +46,7 @@ CREATE TABLE `users` (
   --  Dumping data for table 'users'
 INSERT INTO `users` (`id`, `ip_address`, `username`, `password`, `salt`, `email`, `activation_code`, `forgotten_password_code`, `created_on`, `last_login`, `active`, `first_name`, `last_name`, `company`, `phone`)
 VALUES
-     ('1','127.0.0.1','administrator','$2a$07$SeBknntpZror9uyftVopmu61qg0ms8Qv1yV6FG.kQOSM.9QhmTo36','','admin@admin.com','',NULL,'1268889823','1268889823','1', 'Admin','istrator','ADMIN','0');
+     ('1','127.0.0.1','administrator','$2y$08$FxzXdiqacBzcetL.bj/GD.4opLR/umEzmqFwvqXuG7h3AiWWFx11a','','admin@admin.com','',NULL,'1268889823','1268889823','1', 'Admin','istrator','ADMIN','0');
 
   --
   --  Table structure for table 'users_groups'
